4c170540765e91fc2aa5042299beb9d9761b16c9
Shivam Gupta <153017511+Shivamg0520@users.noreply.github.com>
2025-08-24 09:00:12 +0530

    Update news_aggregator.py

--- news_aggregator.py
+++ news_aggregator.py
@@ -2,20 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-
-
 def save_data():
     with open('news_data.json', 'w') as file:
         json.dump(list_of_news_1, file, indent=4, )
         print("Data saved successfully")
-
 
 def load_data():
     try:
         with open('news_data.json', 'r') as file:
             return json.load(file)
     except FileNotFoundError:
-        # print("File not found")
         return []
 
 list_of_news_1 = load_data()
@@ -30,7 +26,6 @@ def jagran():
     for item in data:
         http = "https://www.jagran.com"
         urls = item.find('a')['href']
-
         # print(urls)
         link2 = http + urls
         # print(link2)
@@ -41,67 +36,46 @@ def jagran():
         for item2 in data2:
             title = item2.find('h1').text.strip()
             # print(f"Headline: {title}\n")
-
             short_summary = item2.find('p', class_='ArticleDetail_ArticleDetail__shortdescription__uLfAR').text.strip()
             # print(f"short summary: {short_summary}\n")
-            
             full_summart = item2.find('div', class_='ArticleBody').text
             # print(f"full summary: {full_summart}\n\n")
-
-
             list_of_news_1.append({'title': title, 'link': link2,'short_summary': short_summary, 'full_summmary': full_summart})
             
-
-
 def aajtak():    
     url = "https://aajtak.in/"
     responce = requests.get(url)
     soup = BeautifulSoup(responce.text, 'html.parser')
     data = soup.find_all('div',class_= 'player-sec')
     # print(data)
-    
     for item in data:
         urls = item.find('a')['href']
-
         # print(urls)
         responce2 = requests.get(urls)
         soup2 = BeautifulSoup(responce2.text,'html.parser')
         data2 = soup2.find_all('div',class_='story-container')
         # print(data2)
-
         for item2 in data2:
             title = item2.find('h1').text.strip()
             # print(f"Headline: {title}\n")
-
             short_summary = item2.find('h2').text.strip()
-            # print(f"short description: {short_summary}\n")
-            
-            # full_summart = item2.find('div', class_='text-formatted')
             soup3 = soup2.find_all('div',class_='text-formatted')
             for item3 in soup3:
                 a = item3.find_all('p')
                 full_summart =''
                 for para in a:
                     full_summart += para.text.strip()
-                    # print(para.text.strip())
-                # print(f"full summary: {a}\n\n")
-
-
             list_of_news_1.append({'title': title, 'link': urls,'short_summary': short_summary, 'full_summmary': full_summart})
 
 def showing_data():
     for news in list_of_news_1:
         print(f"Title:{news['title']}\n\nLink: {news['link']}\n\nShort summary: {news['short_summary']}\n\nFull summary: {news['full_summmary']}\n\n\n")
 
-
 def main():
     jagran()
     aajtak()
     
-            
-
 print("1: Jagran se news scrape karen\n2: Aajtak se news scrape kare\n3. Both\n4: Saved news dekhen\n5: Exit")
-
 while True:
     user_input = input("\nEnter your choice: ")
     if user_input == '1':
@@ -138,6 +112,3 @@ while True:
                 print("Invalid input")
     else:
         print("Invalid input")
-
-
-
